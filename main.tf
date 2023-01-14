a233ef762b93cfa55c551a1b9237fbc48c133d26
dupe <njdupe@gmail.com>
2023-01-14 15:56:32 -0500

    fix: update ec2 instance image

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_vm" {
-  ami           = "ami-065deacbcaac64cf2" //Ubuntu AMI
+  ami           = "ami-06878d265978313ca" // Ubuntu AMI
   instance_type = "t2.micro"
 
   tags = {
